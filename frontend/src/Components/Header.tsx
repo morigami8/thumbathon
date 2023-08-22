550efa3662932d89256ded61cb686a1e40f71198
Morgan Atwood <atwood.morganj@gmail.com>
2023-08-21 21:24:48 -0700

    header updated

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,7 +1,28 @@
+import { Box, Center, HStack, Icon } from '@chakra-ui/react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import React from 'react';
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-portfolio-logo">
+          <img
+            alt="portfolio-logo"
+            src="https://res.cloudinary.com/dl8likjsr/image/upload/v1692677530/portfolio_logo_ovwpfj.jpg"
+          />
+        </div>
+        <HStack gap="24px" spacing="24px" className="header-socials">
+          <a href="https://github.com/morigami8/thumbathon">
+            <Icon width="50px" height="25px" as={FaGithub} />
+          </a>
+          <a href="https://www.linkedin.com/in/morgan-atwood-5b6a8756/">
+            <Icon width="50px" height="25px" as={FaLinkedin} />
+          </a>
+        </HStack>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
