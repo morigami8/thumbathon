933842987f8cfa9b75f9d20dc70f524706b8825f
Morgan Atwood <atwood.morganj@gmail.com>
2023-08-26 13:40:07 -0700

    styling update

--- frontend/src/Components/Content.tsx
+++ frontend/src/Components/Content.tsx
@@ -13,7 +13,7 @@ const Content = () => {
       className="main-content"
       bg="purple.300"
       color="white"
-      background="rgb(34,193,195); background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);"
+      background="rgb(34,193,195); background: linear-gradient(90deg,rgba(253,187,45,1) 0%, rgba(34,193,195,1) 100%);"
       borderRadius={40}
     >
       <Box
